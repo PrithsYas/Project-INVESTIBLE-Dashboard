c615ffbca33e4d4a48bf52bb562d313dc5923485
Prithuyash Joshi <prithudrkyash79@gmail.com>
2025-06-23 00:47:58 +0530

    Set default port for 0.0.0.0

--- app.py
+++ app.py
@@ -174,4 +174,5 @@ def dashboard_view():
         return render_template("dashboard.html", error="Something went wrong.")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
